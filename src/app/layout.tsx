eccdbba9e63110c2631bdf0f55c17c99a46912e1
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-07-23 00:01:15 +0300

    fix: colors change, connection to tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className='bg-background text-foreground font-sans'>
+        {children}
+      </body>
     </html>
   );
 }
